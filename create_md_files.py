ba3015780f9013a36049552a6fe9a942a9ca0290
Sameer Singh <sameeersingh@gmail.com>
2021-09-03 17:43:35 -0700

    update to 2021, add code to download PDFs

--- create_md_files.py
+++ create_md_files.py
@@ -2,12 +2,13 @@ import argparse
 import json
 from pprint import pprint
 import wget
+from urllib import error
 
 def parse_arguments():
   parser = argparse.ArgumentParser(description="Write Markdown pages for each paper")
-  parser.add_argument("--output-md", default="../akbc-2020/pages/proceedings", help="Directory where generated markdown will be stored")
+  parser.add_argument("--output-md", default="../akbc-2021/pages/proceedings", help="Directory where generated markdown will be stored")
   parser.add_argument("--papers", default="papers.json", help="JSON file all the paper info")
-  parser.add_argument("--output-pdf", default="../akbc-2020/assets/pdfs", help="Directory where files will be stored")
+  parser.add_argument("--output-pdf", default="../akbc-2021/assets/pdfs", help="Directory where files will be stored")
   return parser.parse_args()
 
 def write_md(paper, dir):
@@ -24,9 +25,10 @@ header:
 def write_pdf(paper, dir):
   if paper['pdf'] != "":
     url = "https://openreview.net" + paper['pdf']
-    wget.download(url, dir + "/" + paper["forum_id"] + ".pdf")
-
-
+    try:
+      wget.download(url, dir + "/" + paper["forum_id"] + ".pdf")
+    except error.HTTPError as e:
+      print(url)
 
 if __name__ == "__main__":
   args = parse_arguments()
